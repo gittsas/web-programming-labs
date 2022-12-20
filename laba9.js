a3cf9989dce356f2c6401fb03dad43056a2c28af
Тимошенский Саша <timoshenskiy.sasha@mail.ru>
2022-12-20 10:35:51 +0700

    Нажми меня

--- laba9.js
+++ laba9.js
@@ -1,21 +1,34 @@
-function clickMe(button) {
-    if(button.innerHTML = 'Щелчок')
+function Click1(button) {
+    if(button.innerHTML == 'Щелчок')
         button.innerHTML = 'Спасибо';
     else
         button.innerHTML = 'Щелчок'
 }
 
-function clickMe(button) {
-    if(button.innerHTML = 'Имя Артур')
+function Click2(button) {
+    if(button.innerHTML == 'Имя Артур')
         button.innerHTML = 'Фамилия Вайц';
     else
         button.innerHTML = 'Имя Артур'
 }
 
-function clickMe(button){
-    if(button.innerHTML = 'Щелчок')
-        button.innerHTML = 'Фамилия Тимошенский'
+function Click3(button){
+    if(button.innerHTML == 'Имя Александр')
+        button.innerHTML = 'Фамилия Тимошенский';
     else
         button.innerHTML = 'Имя Александр'    
+}
+
+function Click4(button){
+    if(button.innerHTML == 'Нажми меня')
+        button.innerHTML = 'Ещё раз нажми';
+
+    else if(button.innerHTML == 'Ещё раз нажми')
+        button.innerHTML = 'Ещё';
+
+    else if(button.innerHTML == 'Ещё')
+        button.innerHTML = 'Начать сначала';
+    else
+        button.innerHTML = 'Нажми меня'
 }
 
