b90252086b254e6f5b5c48ecc8700b003d68bece
Тимошенский Саша <timoshenskiy.sasha@mail.ru>
2022-12-20 09:55:20 +0700

    Кнопка Имя-Фамилия

---
+++ laba9.js
@@ -0,0 +1,13 @@
+function clickMe(button) {
+    if(button.innerHTML = 'Щелчок')
+        button.innerHTML = 'Спасибо';
+    else
+        button.innerHTML = 'Щелчок'
+}
+
+function clickMe(button) {
+    if(button.innerHTML = 'Имя Артур')
+        button.innerHTML = 'Фамилия Вайц';
+    else
+        button.innerHTML = 'Имя Артур'
+}
